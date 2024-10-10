1d45892f7ed9d520b1a3ef0e32a931d074189468
Rafael Meha <97791234+RafaelMeha@users.noreply.github.com>
2024-10-10 16:26:50 +0100

    Enabled client to add a username

--- client/src/app/hub/hub.component.ts
+++ client/src/app/hub/hub.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocketService } from '../services/socket.service';
+import { HubmodalComponent } from '../modal/hub-modal/hubmodal/hubmodal.component';
 
 @Component({
   selector: 'app-hub',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HubmodalComponent],
   templateUrl: './hub.component.html',
-  styleUrl: './hub.component.css'
+  styleUrls: ['./hub.component.css']
 })
-export class HubComponent {
+export class HubComponent implements OnInit {
+  public clients: string[] = [];
+  public showModal: boolean = true; 
 
+  constructor(private socketService: SocketService) {}
+
+  ngOnInit(): void {
+    this.socketService.clients.subscribe((data: string[]) => {
+      this.clients = data;
+    });
+  }
+
+  onUsernameSet(username: string) {
+    if (username) {
+      this.socketService.setUsername(username);
+      this.showModal = false;
+    }
+  }  
 }
