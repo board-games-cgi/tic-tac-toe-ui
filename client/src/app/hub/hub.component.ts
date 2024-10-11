e0c1eba3053f0ba3cef37596070f160a7b3a42f9
pmzpro <pedromcruz97@gmail.com>
2024-10-11 16:59:52 +0100

    Creating colors for users

--- client/src/app/hub/hub.component.ts
+++ client/src/app/hub/hub.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocketService } from '../services/socket.service';
 import { HubmodalComponent } from '../modal/hub-modal/hubmodal/hubmodal.component';
@@ -13,9 +13,12 @@ import { HubmodalComponent } from '../modal/hub-modal/hubmodal/hubmodal.componen
 export class HubComponent implements OnInit {
   public clients: string[] = [];
   public currentUser: string = 'Player';
-  public showModal: boolean = true; 
+  public showModal: boolean = true;
+  selectedClass: string = 'black-fill';
 
-  constructor(private socketService: SocketService) {}
+  @ViewChild('svg') svg!: ElementRef
+
+  constructor(private socketService: SocketService) { }
 
   ngOnInit(): void {
     this.socketService.clients.subscribe((data: string[]) => {
@@ -29,5 +32,29 @@ export class HubComponent implements OnInit {
       this.socketService.setUsername(username);
       this.showModal = false;
     }
-  }  
+  }
+
+  onUserChangeColor(colorId: number) {
+    switch (colorId) {
+      case 1:
+        this.svg.nativeElement.style.backgroundColor = "purple"
+        break
+      case 2:
+        this.svg.nativeElement.style.backgroundColor = "red"
+        break
+      case 3:
+        this.svg.nativeElement.style.backgroundColor = "blue"
+        break
+      case 4:
+        this.svg.nativeElement.style.backgroundColor = "green"
+        break
+      case 5:
+        this.svg.nativeElement.style.backgroundColor = "black"
+        break
+      case 6:
+        this.svg.nativeElement.style.backgroundColor = "yellow"
+        break
+    }
+  };
+
 }
