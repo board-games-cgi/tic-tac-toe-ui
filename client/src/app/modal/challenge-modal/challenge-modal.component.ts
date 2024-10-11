19746cac1bb8d34596645259a1b4c0e33ffe1b55
Diogo Santos <diogomariasantos3@gmail.com>
2024-10-11 15:32:13 +0100

    Challenge modal created

---
+++ client/src/app/modal/challenge-modal/challenge-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-challenge-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './challenge-modal.component.html',
+  styleUrl: './challenge-modal.component.css'
+})
+export class ChallengeModalComponent {
+
+}
